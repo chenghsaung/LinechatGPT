173dd984075c6727e5efdeb79f467815c2437891
chenghsaung <jim910208@gmail.com>
2023-03-27 00:13:11 +0800

    refine coding style

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ from src.completion_handler import completion_heandler
 
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
-from linebot.models import *
+from linebot.models import (
+    ButtonsTemplate, ImageSendMessage,
+    MessageEvent, PostbackTemplateAction,
+    TemplateSendMessage, TextMessage, URITemplateAction
+)
 
 app = Flask(__name__)
 user_id = os.getenv("OPENAI_API_KEY")
@@ -44,17 +48,17 @@ def callback():
   return 'OK'
 
 
-button_template_message = ButtonsTemplate(
+BUTTONS_TEMPLATE_MESSAGE = ButtonsTemplate(
   thumbnail_image_url="https://i.imgur.com/i8tEmlY.jpg",
   title='chatgpt相關網頁',
   text='選擇動作',
   ratio="1.51:1",
   image_size="cover",
   actions=[
-    #                                PostbackTemplateAction 點擊選項後，
-    #                                 除了文字會顯示在聊天室中，
-    #                                 還回傳data中的資料，可
-    #                                 此類透過 Postback event 處理。
+    # PostbackTemplateAction 點擊選項後，
+    # 除了文字會顯示在聊天室中，
+    # 還回傳data中的資料，可
+    # 此類透過 Postback event 處理。
     URITemplateAction(label='chatGPT指令大全',
                       uri='https://www.explainthis.io/zh-hant/chatgpt'),
     URITemplateAction(label='chatGPT web chat',
@@ -72,23 +76,20 @@ def handle_message(event):
 
   print(f"user: {user_id}, message: {event.message.text}")
   if event.message.text == "/選單":
-    line_bot_api.reply_message(
-      event.reply_token,
-      TemplateSendMessage(alt_text="chatGPT相關網頁",
-                          template=button_template_message))
+    msg = TemplateSendMessage(alt_text="chatGPT相關網頁",
+                          template=BUTTONS_TEMPLATE_MESSAGE)
   elif event.message.text == "/指令":
-    line_bot_api.reply_message(
-      event.reply_token,
-      TextMessage(text=emoji.emojize(
+    msg = TextMessage(text=emoji.emojize(
         "以下是機器人支援的指令:\n" +
         "/角色 :arrow_right:告訴機器人他是什麼角色，有助於產生更好的結果，例如:/角色 你是一位專業股票分析師\n" +
         "/圖片 :arrow_right:依照條件產出圖片，例如:/圖片 給我一張海邊風景圖\n" +
         "/清除 :arrow_right:讓機器人忘掉之前的對話\n" + "/選單 :arrow_right:叫出功能選單",
-        language='alias')))
+        language='alias'))
   elif event.message.text.startswith("/角色"):
     prompt = event.message.text[3:]
     prompts._change_system(user_id=user_id, data=prompt)
-    line_bot_api.reply_message(event.reply_token, TextMessage(text="角色設定完成!"))
+    msg = TextMessage(text=msg)
+    line_bot_api.reply_message(event.reply_token, msg)
 
   elif event.message.text.startswith("/圖片"):
     prompt = event.message.text[3:]
@@ -97,12 +98,12 @@ def handle_message(event):
     image_url = resp['data'][0]['url']
     msg = ImageSendMessage(original_content_url=image_url,
                            preview_image_url=image_url)
-    line_bot_api.reply_message(event.reply_token, msg)
     prompts._append(user_id=user_id, role="assistant", content=image_url)
+    
   elif event.message.text.startswith("/清除"):
     prompt = event.message.text[3:]
     prompts._clear(user_id=user_id)
-    line_bot_api.reply_message(event.reply_token, TextMessage(text="忘記了"))
+    msg = TextMessage(text="忘記了!")
   else:
     prompts._append(user_id=user_id, role="user", content=event.message.text)
     print("====for debug=======")
@@ -111,13 +112,12 @@ def handle_message(event):
     resp = openai.ChatCompletion.create(
       model='gpt-3.5-turbo',
       messages=prompts._output_messages(user_id=user_id))
-
+    msg = TextMessage(text=resp['choices'][0]['message']['content'])
     prompts._append(user_id=user_id,
                     role="assistant",
                     content=resp['choices'][0]['message']['content'])
-    line_bot_api.reply_message(
-      event.reply_token,
-      TextMessage(text=resp['choices'][0]['message']['content']))
+  
+  line_bot_api.reply_message(event.reply_token, msg)
   print(f"最新的messages: {prompts._output_messages(user_id=user_id)}")
 
 
